4eff01676b0c4708002d531cdd9201b76edd7e1c
LinaPenaC <linastefannypc@gmail.com>
2026-04-08 01:41:57 -0500

    Update BestFit

--- src/ur_os/memory/freememorymagament/BestFitMemorySlotManager.java
+++ src/ur_os/memory/freememorymagament/BestFitMemorySlotManager.java
@@ -8,18 +8,38 @@ package ur_os.memory.freememorymagament;
  *
  * @author super
  */
-public class BestFitMemorySlotManager extends FreeMemorySlotManager{
-    
-    public BestFitMemorySlotManager(int memSize){
+public class BestFitMemorySlotManager extends FreeMemorySlotManager {
+
+    public BestFitMemorySlotManager(int memSize) {
         super(memSize);
     }
-    
+
     @Override
     public MemorySlot getSlot(int size) {
-        MemorySlot m = null;
-        //ToDo
-        
-        return m;
+        MemorySlot bestFitSlot = null;
+
+        // se recorre y se valida si se ha encontrado uno "perfecto"
+        for (int i = 0; i < list.size() && (bestFitSlot == null || bestFitSlot.getSize() != size); i++) {
+            MemorySlot currentSlot = list.get(i);
+
+            if (currentSlot.canContain(size)) {
+                if (bestFitSlot == null || currentSlot.getSize() < bestFitSlot.getSize()) {
+                    bestFitSlot = currentSlot;
+                }
+            }
+        }
+
+        if (bestFitSlot != null) {
+            if (bestFitSlot.getSize() == size) {
+                list.remove(bestFitSlot);
+                return bestFitSlot;
+            } else {
+                //  assignMemory actualiza el slot original y devuelve el nuevo
+                return bestFitSlot.assignMemory(size);
+            }
+        }
+
+        System.out.println("Error - Memory cannot allocate a slot big enough for the requested memory");
+        return null;
     }
-    
 }
