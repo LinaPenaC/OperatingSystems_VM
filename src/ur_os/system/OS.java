070fe03e4935a71defbb3b3009b1deece3b579fb
LinaPenaC <linastefannypc@gmail.com>
2026-04-08 01:32:09 -0500

    Menu implementation

--- src/ur_os/system/OS.java
+++ src/ur_os/system/OS.java
@@ -56,7 +56,7 @@ public class OS {
     public static final int MAX_PROCESS_PRIORITY = 10; //Page size in bytes
     public static final int PAGE_SIZE = 64; //Page size in bytes
     public static final MemoryManagerType SMM = MemoryManagerType.CONTIGUOUS;
-    public static final FreeMemorySlotManagerType MSM = FreeMemorySlotManagerType.FIRST_FIT;
+    public static FreeMemorySlotManagerType MSM = FreeMemorySlotManagerType.FIRST_FIT; //Aquí estoy cambiendo el FIT
     
     public static final ProcessVirtualMemoryManagerType PVMM = ProcessVirtualMemoryManagerType.LRU;
     public static final int FRAMES_PER_PROCESS = 3; //Maximum number of frames assigned to a process, if virtual memory is on
